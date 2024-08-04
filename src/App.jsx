2b695293b990e412fb2f85ad4a68a06e22b4ed36
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-08-04 12:52:56 +0300

    dont igredients

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// src/App.jsx
+import './App.css';
+
+const App = () => {
+  const recipe = [
+    { name: 'cheese', available: true },
+    { name: 'eggs', available: false },
+    { name: 'giants eyes', available: true },
+  ];
+
+  return (
+    <>
+      <h1>tasks List</h1>
+      <ul>
+        {recipe.map((ingredient, index) => (
+          <li key={index} className={ingredient.available ? 'completed' : 'not-completed'}>
+            {ingredient.name}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
